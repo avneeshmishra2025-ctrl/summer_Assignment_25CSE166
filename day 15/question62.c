3718a522d2c62b73c39a1f8382000ebf3ed7ff67
avneeshmishra2025-ctrl <avneesh.mishra2025@glbajajgroup.org>
2026-06-23 22:25:00 +0530

    Add files via upload

---
+++ day 15/question62.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, d;
+    printf("Enter number of elements: ");
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Invalid size\n");
+        return 1;
+    }
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter number of positions to rotate left: ");
+    if (scanf("%d", &d) != 1 || d < 0) {
+        printf("Invalid rotation count\n");
+        return 1;
+    }
+
+    d %= n;
+    printf("Array after left rotation by %d:\n", d);
+    for (int i = 0; i < n; i++) {
+        int index = (i + d) % n;
+        printf("%d ", arr[index]);
+    }
+    printf("\n");
+
+    return 0;
+}
