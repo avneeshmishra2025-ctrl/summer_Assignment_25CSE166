a8a5b80872d7a2e8c7829a6881a97cb665d17b45
avneeshmishra2025-ctrl <avneesh.mishra2025@glbajajgroup.org>
2026-06-27 22:56:50 +0530

    Add files via upload

---
+++ day19/question75.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int rows, cols;
+    printf("Enter number of rows and columns: ");
+    if (scanf("%d %d", &rows, &cols) != 2 || rows <= 0 || cols <= 0) {
+        printf("Invalid dimensions.\n");
+        return 1;
+    }
+
+    int matrix[rows][cols];
+    int transpose[cols][rows];
+
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    printf("Transpose of the matrix:\n");
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
